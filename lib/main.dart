18232754438c633131deebe537857c63fef91acc
Your Name <you@example.com>
2026-07-25 16:47:00 +0300

    Fix: Crop 1/3 width (400x250), force fitXY to fill bounds, fix 2014 date logic, add 30m auto-update

--- lib/main.dart
+++ lib/main.dart
@@ -154,7 +154,7 @@ class _TimeOfWarScreenState extends State<TimeOfWarScreen> {
   Timer? _debounce;
 
   final DateTime _date2022Start = DateTime(2022, 2, 24, 5, 0); 
-  final DateTime _date2014Start = DateTime(2014, 2, 19, 12, 0); 
+  final DateTime _date2014Start = DateTime(2014, 2, 20, 12, 0); 
 
   @override
   void initState() {
