7674a4ac0f4e3b26781f91c0f7dd942e9bb5f382
Your Name <you@example.com>
2026-08-30 07:11:10 +0300

    Fix settings gradle structure

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,9 +1,25 @@
 pluginManagement {
+    def flutterSdkPath = {
+        def properties = new Properties()
+        file("local.properties").withInputStream { properties.load(it) }
+        def flutterPath = properties.getProperty("flutter.sdk")
+        assert flutterPath != null, "flutter.sdk not set in local.properties"
+        return flutterPath
+    }()
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "7.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
 dependencyResolutionManagement {
@@ -12,12 +28,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
-}
-
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "7.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
 include(":app")
